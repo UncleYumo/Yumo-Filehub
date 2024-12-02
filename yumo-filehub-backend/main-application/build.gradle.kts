25d57f8305c2cebc556c310ba27a47eb9948c507
uncle_yumo <wy13921971147@163.com>
2024-12-02 11:34:59 +0800

    xxx

--- yumo-filehub-backend/main-application/build.gradle.kts
+++ yumo-filehub-backend/main-application/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "filehub.uncleyumo.cn"
-version = "0.0.1-SNAPSHOT"
+version = "2.0.0"
 
 java {
 	toolchain {
@@ -25,6 +25,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("cn.uncleyumo.utils:print-plus-kotlin:1.1.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
